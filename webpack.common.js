ef9235d5f6a34d4dcef6986cf5d3d1580e593b24
Sorin Cătană <scatana@users.noreply.gitlab.com>
2019-12-13 23:43:15 -0500

    Squashed commit of the following:
    commit 53ca4b2672b05ba61ea47eb83b6787363365e910
    Author: Sorin Cătană <scatana@users.noreply.gitlab.com>
    Date:   Fri Dec 13 21:18:21 2019 -0500
    
        Split webpack config
    
    commit bb239e75a5ebc9de7432a937ea5cbfa97c0084b4
    Author: Sorin Cătană <scatana@users.noreply.gitlab.com>
    Date:   Fri Dec 13 20:55:34 2019 -0500
    
        Added webpack, started building the pipeline

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const INPUT_DIR = path.resolve(__dirname, 'src');
+const OUTPUT_DIR = path.resolve(__dirname, 'public');
+
+module.exports = {
+  context: INPUT_DIR,
+  entry: {
+    main: './app/main.js'
+  },
+  output: {
+    path: OUTPUT_DIR,
+    filename: 'js/[name].js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new CopyPlugin([
+      'favicon.ico',
+      'manifest.json',
+      'service-worker.js',
+      { from: 'css', to: 'css' },
+      { from: 'images', to: 'images' }
+    ])
+  ]
+}
